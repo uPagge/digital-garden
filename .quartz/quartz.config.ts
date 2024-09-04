8c78675bc40f9483afa0e840d9b64aa372925586
Struchkov Mark <mark@struchkov.dev>
2024-09-04 20:41:49 +0300

    Изменение цвета

--- .quartz/quartz.config.ts
+++ .quartz/quartz.config.ts
@@ -33,7 +33,7 @@ const config: QuartzConfig = {
           gray: "#b8b8b8",
           darkgray: "#4e4e4e",
           dark: "#2b2b2b",
-          secondary: "#284b63",
+          secondary: "#2c678d",
           tertiary: "#84a59d",
           highlight: "rgba(143, 159, 169, 0.15)",
           textHighlight: "#fff23688",
