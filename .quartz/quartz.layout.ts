0d8d8815fb612707540b5a1c160fe6328dae5c0c
Struchkov Mark <mark@struchkov.dev>
2024-09-06 01:01:22 +0300

    tags

--- .quartz/quartz.layout.ts
+++ .quartz/quartz.layout.ts
@@ -34,6 +34,7 @@ export const defaultContentPageLayout: PageLayout = {
         Component.Breadcrumbs(),
         Component.ArticleTitle(),
         Component.ContentMeta(),
+        Component.TagList(tagListConfig),
     ],
     left: [
         Component.PageTitle(),
