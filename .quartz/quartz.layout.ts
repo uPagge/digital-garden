837582dfa03c41d4d5f5f1d5d08aa138dd5eab1c
Struchkov Mark <mark@struchkov.dev>
2024-09-06 00:40:04 +0300

    remove yandex metrica

--- .quartz/quartz.layout.ts
+++ .quartz/quartz.layout.ts
@@ -15,14 +15,13 @@ export const sharedPageComponents: SharedLayout = {
             }
         }),
     ],
-    footer: [Component.Footer({
+    footer: Component.Footer({
         links: {
             "About Me": "https://mark.struchkov.dev/cv/",
             "B.log": "https://struchkov.dev",
             "CV": "https://mark.struchkov.dev/cv/?utm_source=gardenru&utm_medium=organic&utm_campaign=static",
         },
     }),
-    ]
 }
 const githubSourceConfig = {
     repoLink: "https://github.com/upagge/digital-garden",
@@ -39,7 +38,7 @@ export const defaultContentPageLayout: PageLayout = {
         Component.PageTitle(),
         Component.MobileOnly(Component.Spacer()),
         Component.Search(),
-        Component.DesktopOnly(Component.RecentNotes({showTags: false, limit: 4})),
+        Component.DesktopOnly(Component.RecentNotes({ showTags: false, limit: 4 })),
         Component.Ads(),
         Component.Darkmode(),
     ],
