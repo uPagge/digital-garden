f2f8786deeb3f06f18b6dad6e66fcdb605c5bc4b
Struchkov Mark <mark@struchkov.dev>
2024-09-18 19:54:17 +0300

    zip_image.sh

--- meta/files/zip_image.sh
+++ meta/files/zip_image.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 file=comp.flag
 
@@ -7,25 +6,25 @@ if [ -f "$file" ]; then
 fi
 
 find ./images/ -type f -not -path "./images/comp/*" ! -name "*-no-comp.*" $option -iname "*.png" -exec sh -c '
-  png_file="${1/\/images\//\/images\/comp\/}"
+  png_file="./images/comp/${1#*/images/}"
   png_dir="$(dirname "$png_file")"
   mkdir -p "$png_dir"
-  cp "$1" "${png_file}"
-  optipng -o7 "${png_file}"
-  advpng -z4 "${png_file}"
-  pngcrush -rem gAMA -rem alla -rem cHRM -rem iCCP -rem sRGB -rem time -ow "${png_file}"
+  cp "$1" "$png_file"
+  optipng -o7 "$png_file"
+  advpng -z4 "$png_file"
+  pngcrush -rem gAMA -rem alla -rem cHRM -rem iCCP -rem sRGB -rem time -ow "$png_file"
 ' _ {} \;
 
 find ./images/ -type f -not -path "./images/comp/*" ! -name "*-no-comp.*" $option -iregex '.*\.\(jpg\|jpeg\)' -exec sh -c '
-  jpg_file="${1/\/images\//\/images\/comp\/}"
+  jpg_file="./images/comp/${1#*/images/}"
   jpg_dir="$(dirname "$jpg_file")"
   mkdir -p "$jpg_dir"
-  cp "$1" "${jpg_file}"
-  jpegoptim --all-progressive "${jpg_file}"
+  cp "$1" "$jpg_file"
+  jpegoptim --all-progressive "$jpg_file"
 ' _ {} \;
 
 find ./images/comp -type f -iregex '.*\.\(jpg\|jpeg\|png\)' -not -iregex '.*no-comp\.\(jpg\|jpeg\|png\)' $option -exec sh -c '
-  webp_file="${1/\/images\/comp\//\/images\/webp\/}"
+  webp_file="./images/webp/${1#*/images/comp/}"
   webp_dir="$(dirname "$webp_file")"
   mkdir -p "$webp_dir"
   cwebp -mt -af -progress -m 6 -q 75 -pass 10 "$1" -o "${webp_file%.*}.webp"
